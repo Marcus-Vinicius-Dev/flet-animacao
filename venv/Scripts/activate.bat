1dbeed66274c0c7b701bbb5d26bb6a40558fefdf
Marcus-Vinicius-C-S <ciapdivtectic@gmail.com>
2024-04-29 20:58:28 -0300

    exclusão e criação da venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\marcus.silva\OneDrive - PRODESP\Área de Trabalho\flet\flet-animacao\venv
+set VIRTUAL_ENV=C:\Users\vinic\Desktop\flet\flet-animacao\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
